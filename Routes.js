16f79873420179b965442af76096b9348eee244d
VennilaPugazhenthi <vennilapugazhenthi@gmail.com>
2021-03-06 09:58:19 -0500

    Update tabIcon

--- Routes.js
+++ Routes.js
@@ -7,47 +7,32 @@ import { Feather, FontAwesome5 } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
-const tabOptions = {
-  tabBarOptions: {
-    activeTintColor: "black",
-    inactiveTintColor: "grey",
-    showIcon: true,
-    upperCaseLabel: false,
-    style: {
-      backgroundColor: "white",
-    },
-    labelStyle: {
-      fontSize: 15,
-    },
-    indicatorStyle: {
-      backgroundColor: "black",
-    },
-    pressColor: "blue",
-  },
-  lazy: true,
-};
-
 export const MainStack = () => {
   return (
-    <Tab.Navigator {...tabOptions} tabBarOptions={{ showIcon: true }}>
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          tabBarIcon: ({ tintColor }) => (
-            <Feather name="home" size={24} color={tintColor} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Recordings"
-        component={RecordingScreen}
-        options={{
-          tabBarIcon: ({ tintColor }) => (
-            <FontAwesome5 name="record-vinyl" size={24} color={tintColor} />
-          ),
-        }}
-      />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconColor;
+          if (route.name === "Home") {
+            iconColor = focused ? "blue" : "grey";
+            return <Feather name="home" size={24} color={iconColor} />;
+          }
+          if (route.name === "Recordings") {
+            iconColor = focused ? "blue" : "grey";
+            return (
+              <FontAwesome5 name="record-vinyl" size={24} color={iconColor} />
+            );
+          }
+        },
+      })}
+      tabBarOptions={{
+        showIcon: true,
+        activeTintColor: "blue",
+        inactiveTintColor: "grey",
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Recordings" component={RecordingScreen} />
     </Tab.Navigator>
   );
 };
